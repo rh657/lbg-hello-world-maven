1c9b636dc9d6a3d29c6669483ef963e73d15fe88
Rachel <rachel@example.com>
2025-02-04 14:17:53 +0000

    changed names

--- src/main/java/com/qa/App.java
+++ src/main/java/com/qa/App.java
@@ -9,9 +9,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println(sayHello());
-        System.out.println(sayHelloToSomeone("Alice"));
+        System.out.println(sayHelloToSomeone("Rachel"));
         System.out.println(sayHelloToSomeone("Bob"));
-        System.out.println(sayHelloToSomeone("Charlie"));
+        System.out.println(sayHelloToSomeone("Sigi"));
         System.out.println(sayHelloToSomeone("Zena"));
         System.out.println(sayGoodbye());
     }
